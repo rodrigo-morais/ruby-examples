2d59fe289119fe77aa165e0f56bef05783045faf
Rodrigo Morais <morais.rm@gmail.com>
2015-06-19 06:37:06 -0300

    Adding new examples of Ruby.

---
+++ class_method.rb
@@ -0,0 +1,17 @@
+class Calculator
+  
+  def double(val)
+    val * 2
+  end
+
+  def multiply_three(val)
+    val * 3
+  end
+
+end
+
+calc = Calculator.new
+
+puts "Double of 5 is #{calc.double(5)}"
+
+puts "Triple of 5 is #{calc.multiply_three(5)}"
